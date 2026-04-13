91db575be0ec2a0dec6fde1119dfab63f8d9806c
Zach Metcalf <zachmetcalf@gmail.com>
2026-04-13 09:02:30 -0700

    standards

--- scripts/standards/standards.bat
+++ scripts/standards/standards.bat
@@ -17,7 +17,6 @@ if not exist "%projectdir%\.vscode" (
 )
 
 copy /y "%standardsdir%\.vscode\settings.json" "%projectdir%\.vscode\settings.json" >nul
-copy /y "%standardsdir%\.vscode\tasks.json" "%projectdir%\.vscode\tasks.json" >nul
 copy /y "%standardsdir%\.clang-tidy" "%projectdir%\.clang-tidy" >nul
 copy /y "%standardsdir%\.editorconfig" "%projectdir%\.editorconfig" >nul
 copy /y "%standardsdir%\.gitattributes" "%projectdir%\.gitattributes" >nul
